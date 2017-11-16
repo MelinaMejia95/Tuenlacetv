6aa38207886a15fc280c345e6b851c9cadadbfe9
Melina Mejia Bedoya <melinamejia95@gmail.com>
2017-11-15 22:37:15 -0500

    Modals created in Barrios Component

--- src/app/barrios/barrios.component.ts
+++ src/app/barrios/barrios.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare let jQuery: any;
+
 @Component({
   selector: 'app-barrios',
   templateUrl: './barrios.component.html',
@@ -10,6 +12,10 @@ export class BarriosComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    jQuery('select').material_select(); 
+    jQuery('.modal').modal();
+    
   }
 
 }
